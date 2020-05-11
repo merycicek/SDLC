e5f16496df7f332435faaedc635c8dd32e387433
Meryem Elibal <merycicek@gmail.com>
2020-05-11 22:50:45 +0000

    added changes

--- variable.tf
+++ variable.tf
@@ -11,9 +11,9 @@ variable "s3_folder_type" {}
 variable "s3_tfstate_file" {} 
 variable "region" {}
 
-variable "public_subnet1" {}
-  variable "public_subnet2" {}
-  variable "public_subnet3" {}
-  variable "private_subnet1" {}
-  variable "private_subnet2" {}
-  variable "private_subnet3" {}
+variable "public_cidr1" {}
+  variable "public_cidr2" {}
+  variable "public_cidr3" {}
+  variable "private_cidr1" {}
+  variable "private_cidr2" {}
+  variable "private_cidr3" {}
