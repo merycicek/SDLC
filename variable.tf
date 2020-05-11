3303720f42f574dff41f714ddedb5ec1ff3f5bbc
Meryem Elibal <merycicek@gmail.com>
2020-05-11 22:45:55 +0000

    added subnets in variable

--- variable.tf
+++ variable.tf
@@ -10,3 +10,10 @@ variable "s3_folder_type" {}
 
 variable "s3_tfstate_file" {} 
 variable "region" {}
+
+variable "public_subnet1" {}
+  variable "public_subnet2" {}
+  variable "public_subnet3" {}
+  variable "private_subnet1" {}
+  variable "private_subnet2" {}
+  variable "private_subnet3" {}
