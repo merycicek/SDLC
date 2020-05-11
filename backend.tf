fd827dabfc23be95647f8762c1b04f17638fe62e
Meryem Elibal <merycicek@gmail.com>
2020-05-11 22:14:57 +0000

    added qa file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform { 
 backend "s3" { 
-bucket = "sdlc-mery-dev" 
-key = "vet/us-east-1/sharedtools/dev/infrastructure.tfstate" 
-region = "us-east-1" 
+bucket = "sdlc-mery-qa" 
+key = "vet/us-east-2/sharedtools/qa/infrastructure.tfstate" 
+region = "us-east-2" 
   } 
 } 
